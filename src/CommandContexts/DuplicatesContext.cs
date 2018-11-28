07ac2cafb0f022cbf05443a4b4eaee19cdd8a278
Jim Dale <26042891+jim-dale@users.noreply.github.com>
2018-11-28 18:41:46 +0000

    Removed Compare & Moved commands and add Duplicates command

---
+++ src/CommandContexts/DuplicatesContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace FolderCompare
+{
+    using System.Collections.Generic;
+
+    public class DuplicatesContext
+    {
+        public IMetadataSource Source { get; set; }
+
+        public IComparisonReport Report { get; set; }
+
+        public IEnumerable<FileMetadata> Items { get; set; }
+    }
+}
---
+++ src/Commands/DuplicatesCommand.cs
@@ -0,0 +1,51 @@
+﻿
+namespace FolderCompare
+{
+    using McMaster.Extensions.CommandLineUtils;
+    using System;
+    using System.Linq;
+
+    public class DuplicatesCommand
+    {
+        private const DisplayMode DefaultDisplayMode = DisplayMode.All;
+
+        public CommandOption InputOption { get; private set; }
+
+        public DuplicatesContext Context { get; private set; }
+
+        public void Configure(CommandLineApplication<DuplicatesCommand> cmd)
+        {
+            cmd.HelpOption("-?|--help");
+
+            InputOption = cmd.Option("-i|--input <PATH>", "Path to the folder or catalogue to search for duplicate files.", CommandOptionType.SingleValue)
+                .IsRequired()
+                .Accepts(v => v.ExistingFileOrDirectory());
+
+            cmd.OnExecute((Func<int>)OnExecute);
+        }
+
+        private int OnExecute()
+        {
+            Context = new DuplicatesContext
+            {
+                Source = Helpers.GetMetadataSource(Helpers.ExpandPath(InputOption.Value())),
+            };
+
+            Context.Report = new ConsoleComparisonReport(DisplayMode.All);
+
+            Context.Items = Context.Source.GetAll();
+
+            var duplicates = Context.Items.GetDuplicates(i => i.ContentsHash);
+            foreach (var group in duplicates)
+            {
+                Console.WriteLine(group.First().FileName);
+                foreach (var item in group)
+                {
+                    Console.WriteLine($"\t{item.RelativePath}");
+                }
+            }
+
+            return ExitCode.Okay;
+        }
+    }
+}
